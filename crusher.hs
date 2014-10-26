3e57c1485881ef9f2edc2cfda78b859af8c80abc
Linh Phan <Linh@dhcp-206-87-112-206.ubcsecure.wireless.ubc.ca>
2014-10-26 15:54:32 -0700

    VERY ROUGH COPY OF CRUSHER

--- crusher.hs
+++ crusher.hs
@@ -1,3 +1,20 @@
 -- CPSC312 Project 1
 -- Bryan Tai and Linh Phan
---
+ 
+
+
+-- function creates a board that takes in strings and an integer and splits accordingling by breaking
+-- it into sublists of strings where each string will represent a row on the board 
+
+board :: String -> Int -> [String]
+board strings n
+	| (length strings) = 0					= []
+	| otherwise								= (splitAt (n strings))        -- creates a tuple
+
+
+
+	    * * W W W 
+		* - W W - 
+		- - - - -
+		- B B - *
+		B B B * *
