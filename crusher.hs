01ecae05fd366a0ca7e515fa99008ce9c7f686a6
Bryan Tai <redhatpieman@gmail.com>
2014-11-06 13:25:10 -0800

    Still working on stateSearch

--- crusher.hs
+++ crusher.hs
@@ -55,7 +55,7 @@ crusher_o7m8 history side searchdepth n =
 		   side
 		   searchdepth
 		   n))
-
+--also check if inital Board is gameOver
 
 
 
@@ -106,20 +106,66 @@ generateDownLeftMoves
 -BB-*
 BBB**
 
--- Generates best new Board state given game history information
+-- Generates the best new Board state given game history information
 -- Uses the MINMAX algorithm up to a certain searchdepth
 -- First generates all the possible moves up til searchdepth
 -- (not considering those that are repeats)
--- 
+-- Called on EACH possible board.
+-- Initial call, findMax is True. Alternates each level down
 
-stateSearch :: [Board] -> [Board] -> Char -> Int -> Int -> [Board]
-stateSearch unexplored history side searchdepth n path
+stateSearch :: [Board] -> [Board] -> Char -> Int -> Int -> Bool -> Board
+stateSearch unexplored history side searchdepth n findMax
      | (null unexplored) || (searchdepth == 0)   = []
-     | isWin
- (head unexplored)  =  
-     |
+     | 
+
+-- find the best board of the next moves
+	getMinMaxBoard (generateNewMoves (head unexplored) history side)
+		       side --or otherSide?
+		       n
+		       findMax
+		       (getMinMaxScore genBoards side n (if findMax 
+		       		       		      	then maximum 
+							else minimum))
+		       
+--recursive call
+    (stateSearch (tail unexplored) 
+     		 ((head unexplored):history)
+     		 (otherSide side) 
+		 (searchDepth-1))
+		 n
+		 (not findMax)
 
 
+
+
+
+-- Takes in a list of generated boards and returns the BOARD with either
+-- the highest or lowest calculated score based on minmax heuristic.
+-- minmax is either minimum or maximum
+getMinMaxBoard :: [Board] -> Char -> Int -> Bool -> Int -> Board 
+getMinMaxBoard genBoards side n findMax score =
+     | 
+
+-}
+
+
+-- VVVVV TO TEST VVVVV
+
+
+-- Takes in a list of generated boards and returns either
+-- the highest or lowest calculated SCORE based on minmax heuristic.
+-- minmax is either minimum or maximum
+getMinMaxValue :: [Board] -> Char -> Int -> ([Int] -> Int) -> Int 
+getMinMaxValue genBoards side n minmax =
+    minmax (map k genBoards)    
+    where k = (boardEvaluator side n)
+
+-- Checks whether a board is already over due to one side having
+-- less than n pawns
+gameOver :: Board -> Int -> Bool
+gameOver initBoard n = 
+     (isCrushedWin initBoard whitePawn n) ||
+     (isCrushedWin initBoard blackPawn n)
 
 -- Static Board Evaluator
 -- Takes in a Board and relevant information. Returns a score based
@@ -128,23 +174,22 @@ stateSearch unexplored history side searchdepth n path
 -- Will not be passed a board that has already been seen by history
 -- Whoever calls this function should take care of that maybe
 -- <post heuristic here>
-boardEvaluator :: Board -> [Board] -> Char -> Int -> Int
-boardEvaluator initBoard history side n 
-     | (isCrushedWin initBoard side n)		   =  100
-     | (isCrushedWin initBoard (otherSide side) n) = -100
-     | otherwise     	= calculateScore initBoard history side
+boardEvaluator :: Char -> Int -> Board -> Int
+boardEvaluator side n initBoard 
+     | (isCrushedWin initBoard side n)		   =  1000
+     | (isCrushedWin initBoard (otherSide side) n) = -1000
+     | otherwise     	= calculateScore initBoard side 
+
 
 -- (# of given side's pawns times 10) minus (enemy pawns times 10)
 --
-calculateScore :: Board -> [Board] -> Char -> Int
-calculateScore initBoard history side n =
+-- TODO check for adjacent pawns
+-- TODO check if pawns in middle / edge
+calculateScore :: Board -> Char -> Int
+calculateScore initBoard side =
     ((getPawnsForSide initBoard side) * 10) -
     ((getPawnsForSide initBoard (otherSide side)) * 10)
 
--}
-
-
--- VVVVV TO TEST VVVVV
 
 -- Takes in a Board with length n and reverses the orientation of the asterisks
 -- for UR and DL movements.
